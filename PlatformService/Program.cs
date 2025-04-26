4618e35c5a14876ff31b6f981260dd8fbe213cb2
Nitisg Goel <303186@eneco.com>
2025-04-26 18:39:52 +0200

    setting up sql database for production

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -4,19 +4,32 @@ using PlatformService.Data;
 using PlatformService.SyncDataServices.Http;
 
 var builder = WebApplication.CreateBuilder(args);
-
+IConfiguration configuration = app.Services.GetRequiredService<IConfiguration>();
+var _env = builder.Environment;
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddControllers();
 builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 
+
+if (_env.IsProduction())
+{
+    Console.WriteLine("--> Using SqlServer Db");
+    builder.Services.AddDbContext<AppDbContext>(opt =>
+        opt.UseSqlServer(configuration.GetConnectionString("PlatformsConn")));
+}
+else
+{
+    Console.WriteLine("--> Using InMem Db");
+    builder.Services.AddDbContext<AppDbContext>(opt =>
+        opt.UseInMemoryDatabase("InMem"));
+}
+
 var app = builder.Build();
-IConfiguration configuration = app.Services.GetRequiredService<IConfiguration>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
